0beb17803729baae6ae60d7a75ca201b700225e1
limajeferson <jefersonlima@outlook.com>
2022-01-04 20:00:54 -0300

    Exercício1

--- exercises/bin/1_lists_lists_lists/lists_lists_lists.dart
+++ exercises/bin/1_lists_lists_lists/lists_lists_lists.dart
@@ -9,15 +9,86 @@ void main() {
   // Lista D: lista que possua todos os elementos de C mais duas posições no
   // final, o número 2 e 3.
 
+  List listA = [];
+  List listB = [2.2, 2.2, 2.2, 2.2];
+  final listC = listB;
+  List listD = [...listC, 2, 3];
+
+  /* CORREÇÃO
+
+  final listA = list.empty(grownable: true);
+  final listB = list.filled(4, 2.2);
+  final listC = list.unmodfiable(listB);
+  final listD = [...listC, 2, 3];
+
+  */
+
   // 2. Concatenar a Lista A e Lista C salvando o resultado na lista E.
 
-  // 3. Criar Lista F, onde se Lista E tiver tamanho 4, o primeiro número é o 
+  List listE = [...listA, ...listC];
+
+  /* CORREÇÃO
+
+  final listE = listA + listC;
+  
+  */
+
+  // 3. Criar Lista F, onde se Lista E tiver tamanho 4, o primeiro número é o
   // 2.2. Posições seguintes: 5 e 10.
+
+  List listF = [if (listE.length == 4) 2.2, 5, 10];
+
+  /* CORREÇÃO
+
+  final listF = [if (listE.length == 4) 2.2, 5, 10];
+  
+  */
 
   // 4. Exibir na tela se Lista E é igual a Lista B.
 
+  print(listE == listB);
+
+  /* CORREÇÃO
+
+  print(listE == listB);
+  
+  */
+
   // 5. Deletar o conteúdo da Lista D.
 
-  // 6. Exibir na tela se o primeiro elemento da lista F é igual ao último 
+  listD.clear();
+
+  /* CORREÇÃO
+
+  listD.clear(); ou .remove~
+  
+  */
+
+  // 6. Exibir na tela se o primeiro elemento da lista F é igual ao último
   // elemento da Lista C.
+
+  print(listF[0] == listC[listC.length - 1]);
+
+  /* CORREÇÃO
+
+  print(listF.first == listC.last);
+  
+  */
+
+  // Bônus
+
+  List listG = List.from(listF);
+  listF.add(5);
+
+  print(listF);
+  print(listG);
+
+  /* CORREÇÃO
+
+  final listG = listF;
+  listF.add(5);
+  print(listF);
+  print(listG);
+  
+  */
 }
